fe851863a74d2322ead303f7162a96d758f6d5bd
Aurelius <akinpeluifeoluwa007@gmail.com>
2023-10-28 07:17:07 +0100

    Started development

---
+++ ludus_hub/app/page.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar.jsx'
+//import { useEffect, useState } from 'react'
+
+export default function Home({gameData}) {
+  
+   
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+
+}
+
