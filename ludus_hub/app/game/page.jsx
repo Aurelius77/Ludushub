d4f4ac57b7e09ec5d4fff45922041b0606e14e15
Aurelius <akinpeluifeoluwa007@gmail.com>
2023-11-05 16:50:38 +0100

    newest updates

--- ludus_hub/app/game/page.jsx
+++ ludus_hub/app/game/page.jsx
@@ -2,11 +2,13 @@
 import { useGlobalState } from "../context/context"
 import { fetchDataFromAPI } from "../http/api"
 import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
 
 
 export default function Game(){
+  const router = useRouter()
    const apiKey = process.env.API_KEY
    const {state} = useGlobalState()
    const publisherData = state.data || {}
@@ -17,7 +19,7 @@ export default function Game(){
    useEffect(()=>{
       async function fetchData(){
         try{
-        const result = await fetchDataFromAPI( `https://api.rawg.io/api/games/${id}?key=${apiKey}`)
+        const result = await fetchDataFromAPI( `https://api.rawg.io/api/games/662316?key=${apiKey}`)
         setData([result])
         console.log(result)
         console.log('data: ',  data)
@@ -34,16 +36,61 @@ export default function Game(){
     setVisibility(!visibility)
    }
 
+   function redirectUser(url){
+    router.push(`https://${url}`)
+   }
+
   return(
     <>
     <Navbar toggleSidebar={toggleSidebar}/>
   <div className="flex">
     <Sidebar visibility={visibility}/>
     {!visibility && data.length > 0 ? <div className="flex flex-col">
-      <div className="flex justify-between">
+      <div className="md:flex justify-between p-2">
         <div className="img-cont">
-          <p>{data[0].name}</p>
+          <img src={data[0].background_image} alt='poster'></img>
         </div>
+        <div className="game-info flex flex-col md:p-2">
+          <h1 className="text-2xl mb-2 mt-2 md:mt-0">{data[0].name}</h1>
+          <p className="mb-2">{data[0].description_raw}</p>
+          <h1 className="mb-2">Available on this platforms:</h1>
+          <div className="platforms flex mb-3">
+            { data[0].parent_platforms.map((platform, index)=>{
+              return <p key={index} className="border p-1">{platform.platform.name}</p>
+            })}
+          </div>
+          <h1 className="mb-2">Genres:</h1>
+          <div className="platforms flex mb-3">
+            { data[0].genres.map((genre, index)=>{
+              return <p key={index} className="border p-1">{genre.name}</p>
+            })}
+          </div>
+
+          <h1 className="mb-2">Developed by:</h1>
+          <div className="platforms flex mb-3">
+            { data[0].developers.map((developer, index)=>{
+              return <p key={index} className="border p-1">{developer.name}</p>
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="extra-info ml-2">
+         <h1 className="text-xl">{data[0].name} can be gotten from:</h1>
+          {data[0].stores.map((store, index)=>{
+            return <div key={index} className="w-full md:w-1/2 lg:w-1/4 p-3" onClick={()=>redirectUser(store.store.domain)}>
+  <div className="rounded-md shadow-md border border-gray-300 hover:shadow-lg hover:border-gray-400 transition-transform transform hover:scale-105">
+    <div className="rounded-md overflow-hidden">
+      <img
+        src={store.store.image_background}
+        alt=""
+        className="w-full h-40 object-cover"
+      />
+    </div>
+    <h1 className="text-xl mt-2 mb-2 p-3">{store.store.name}</h1>
+  </div>
+</div>
+          })}
       </div>
 
     </div> 
